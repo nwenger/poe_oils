c44dded0c96dab78ba4e2392b7cc4a43c4c4344b
Neil <na>
2023-10-09 23:34:01 -0400

    I got a linter

--- oilapi.py
+++ oilapi.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python
+"""
+API for storing and retrieving data regarding POE Oils and anointments
+"""
+import json
 import os
 import urllib.request
 import requests
 import bs4
-import json
 
-from flask import Flask, request, Response, jsonify
-from flask_cors import CORS, cross_origin
+from flask import Flask, Response, jsonify
+from flask_cors import CORS
 from flask_restful import Resource, Api
 
-oil_wiki='https://pathofexile.fandom.com/wiki/Oil#List_of_anointments'
-oil_output='oil.list'
-recipe_output='recipe.list'
+#FIXME not sure if i need cors stuff still
+#TODO decent handling of filepaths
+#TODO determine worth of bs4
+#TODO get rid of Recipe class
+#TODO differentiate between ring and amulet recipes
+#       and maps i think
+
+OIL_WIKI='https://pathofexile.fandom.com/wiki/Oil#List_of_anointments'
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -19,54 +27,58 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 api = Api(app)
 
 def url_to_soup(url):
-	try:
-		req = urllib.request.Request(url, headers={ 'User-Agent': 'Mozilla/5.0' })
-		html = urllib.request.urlopen(req).read()
-		soup = bs4.BeautifulSoup(html, 'html.parser')
-	except urllib.error.HTTPError:
-		#try again if it fails to fetch
-		time.sleep(5)
-		req = urllib.request.Request(url, headers={ 'User-Agent': 'Mozilla/5.0' })
-		html = urllib.request.urlopen(req).read()
-		soup = bs4.BeautifulSoup(html, 'html.parser')
-	return soup
+    """ convert url to beautiful soup. possibly unneeded """
+    req = urllib.request.Request(url, headers={ 'User-Agent': 'Mozilla/5.0' })
+    html = urllib.request.urlopen(req).read()
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    return soup
 
 class OilList(Resource):
+    """ get or update list of oils """
 
     @staticmethod
     def update_oil_db(csv_path):
         """ truncate local oil list with content from wiki """
-        soup = url_to_soup(oil_wiki)
+        soup = url_to_soup(OIL_WIKI)
         soup_oils = soup.find_all('span', attrs={'class':'c-item-hoverbox__activator'})
         oil_list = {}
         for oil in soup_oils:
             name = oil.find('a').getText()
-            if name in oil_list.keys() or not name.endswith('Oil'):
+            #/if not name.endswith('Oil') or name in oil_list.keys():
+            if not name.endswith('Oil') or name in oil_list:
                 continue
             image = oil.find('img').get('data-src')
             if image is not None:
                 full_image = image[0:image.index('.png') + 4]
-                data = requests.get(full_image).content
+                data = requests.get(full_image, timeout=30).content
                 filename = 'assets/' + name.replace(" ", "_") + ".png"
                 with open(filename, "wb") as f:
                     f.write(data)
                 oil_list[name] = filename
-        with open(csv_path, "w+") as f:
-            for o in oil_list:
+        with open(csv_path, "w+", encoding="utf-8") as f:
+            for o in oil_list.items():
                 f.write(o + '|' + oil_list[o] + "\n")
 
     def post(self):
+        """ update local list of oils """
         self.update_oil_db('oil.list')
         return Response(200)
 
     @staticmethod
     def get():
+        """ get array of oils in following json format
+            {
+                "name": "Something Oil",
+                "image_src": "some/file/path.png"
+            }
+        """
         json_str = '{ "oils": ['
-        with open('oil.list', "r") as oils:
+        with open('oil.list', "r", encoding="utf-8") as oils:
             for line in oils:
                 fields = line.split('|')
                 print(fields)
-                json_str = json_str + '{"name":"' + fields[0] + '", "image_src":"' + fields[1].strip() + '"},'
+                json_str = json_str + '{"name":"' + fields[0] + \
+                    '", "image_src":"' + fields[1].strip() + '"},'
         rv_json = json_str.rstrip(',') + ']}'
         response = jsonify(json.loads(rv_json))
         response.headers.add('Access-Control-Allow-Origin', '*')
@@ -75,26 +87,28 @@ class OilList(Resource):
 
 
 class RecipeList(Resource):
+    """ get or update POE oil recipes """
 
     class Recipe():
+        """ structure for reorganizing anoint recipe into usable json """
         requires = []
         reward_name = ""
         reward_url = ""
         reward_details = ""
 
         def to_json_str(self):
+            """ create json string for json.loads() """
             return '{ "requires": ' + str(self.requires).replace("'",'"') + \
                   ', "reward_name": "' + self.reward_name + \
                   '", "reward_src": "' + self.reward_url + \
                   '", "reward_details": "' + self.reward_details + '" }'
 
     def write_recipes(self, file, table):
+        """ write anoint recipes from html table object to file """
         rows = table.find_all('tr')
         for row in rows:
-            recipe = self.Recipe()
             columns = row.find_all('td')
             recipe_str = ''
-            last_col = ''
             for col in columns:
                 links = col.find_all('a')
                 if len(links) == 0:
@@ -106,8 +120,7 @@ class RecipeList(Resource):
                         recipe_str = recipe_str + obj.getText() + '|'
                     img = obj.find('img')
                     if img is not None:
-                        #TODO just download and save
-                        data = requests.get(img.get('data-src')).content
+                        data = requests.get(img.get('data-src'), timeout=30).content
                         filename = 'assets/' + col.getText().replace(" ", "_") + ".png"
                         with open(filename, "wb") as f:
                             f.write(data)
@@ -116,20 +129,28 @@ class RecipeList(Resource):
 
     def update_recipe_db(self, csv_path):
         """ truncate local recipe.list with content from wiki """
-        source = requests.get('https://pathofexile.fandom.com/wiki/List_of_anointments').text
+        anoint_url = 'https://pathofexile.fandom.com/wiki/List_of_anointments'
+        source = requests.get(anoint_url,timeout=30).text
         soup = bs4.BeautifulSoup(source, "lxml")
         tables = soup.find_all('table')
-        with open(csv_path, "w+") as f:
+        with open(csv_path, "w+", encoding="utf-8") as f:
             for t in tables:
                 self.write_recipes(f, t)
 
     def post(self):
+        """ update local store of oil anointments """
         self.update_recipe_db('recipe.list')
         return Response(200)
 
     def get(self):
+        """ get json of possible oil anointments in following format
+        { "requires": ['Some Oil', 'Another Oil', 'More Oil']
+          "reward_name": name of passive point
+          "reward_src": path/to/image.png
+          "reward_details": description of passive point
+        """
         json_str = '{ "recipes": ['
-        with open('recipe.list', "r") as recipes:
+        with open('recipe.list', "r", encoding="utf-8") as recipes:
             for line in recipes:
                 if not line.strip():
                     continue
@@ -166,6 +187,11 @@ class RecipeList(Resource):
 
 api.add_resource(OilList, '/oils')
 api.add_resource(RecipeList, '/recipes')
+
+try:
+    os.mkdir('assets/')
+except FileExistsError:
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True, host="127.0.0.1", port=9090)
